5eee94a7a452972e8acae551aeda0f253ee1ee8d
ManishKarki1997 <manishkarki247@gmail.com>
2020-09-21 21:56:13 +0545

    setup event for challenge and challenge response

--- sockets/socket.js
+++ sockets/socket.js
@@ -11,11 +11,16 @@ const sockets = (io) => {
         };
       }
       io.emit("ONLINE_USERS", onlineUsers);
+      io.to(socket.id).emit("MY_SOCKET_ID", socket.id)
     });
 
     socket.on("SOMEONE_CHALLENGED_FOR_A_GAME", (data) => {
       io.to(data.challengedUser.socketId).emit("SOMEONE_CHALLEGED_YOU", data);
     });
+
+    socket.on("CHALLENGE_DECISION", data=>{
+      io.to(data.challenger.socketId).emit("CHALLENGE_DECISION", data)
+    })
 
     socket.on("disconnect", () => {
       delete onlineUsers[socket.email];
